c2255d3f54a3d72f59612386085b43dd47b6e87f
tigawanna <denniskinuthiaw@gmail.com>
2022-05-31 14:59:08 +0300

    added initial boiler-plate

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,22 @@
-console.log('Hello World')
+const express = require("express");
+
+const authRoute = require("./routes/auth");
+const goalsRoute = require("./routes/goals");
+
+const PORT = 4000;
+const app = express();
+
+app.use(express.json());
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+res.render("home");
+});
+
+app.use("/auth", authRoute);
+app.use("/goals", goalsRoute);
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running at http://localhost:${PORT}`);
+});
