80fa16a28bee1efe10549afc76777d3224a957ca
devephy <litephantus@gmail.com>
2022-06-10 16:54:01 +0300

    controller configurations

--- Backend/routes/goalRoutes.js
+++ Backend/routes/goalRoutes.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const{getGoals} =require('./../controllers/goalController');
-const{addGoal} =require('./../controllers/goalController');
-const{updateGoal} =require('./../controllers/goalController');
-const{deleteGoal} =require('./../controllers/goalController');
-
-router.get('/', getGoals);
-
-router.post('/', addGoal);
-
-router.put('/:id', updateGoal);
-
-router.delete('/:id', deleteGoal);
-
-module.exports = router
-
-
+const{
+    getGoals, 
+    addGoal,
+    updateGoal, 
+    deleteGoal} =require('./../controllers/goalController');
+    
+    
+    router.get('/', getGoals);
+    
+    router.post('/', addGoal);
+    
+    router.put('/:id', updateGoal);
+    
+    router.delete('/:id', deleteGoal);
+    
+    module.exports = router
