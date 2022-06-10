c2533ed609c5b47630adf493c86b9d23ddb70a68
devephy <litephantus@gmail.com>
2022-06-10 15:53:12 +0300

    routes configurations

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,16 @@
-console.log('Hello World')
+const express = require('express');
+const dotenv = require('dotenv');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use('/api/goals', require('./routes/goalRoutes'));
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
